bb28ca3db62252f3b851150922232a14e13821e8
cal2195 <cmartin7@tcd.ie>
2016-03-19 12:59:18 +0000

    Testing UDT!

--- src/blockswarm/BlockSwarm.java
+++ src/blockswarm/BlockSwarm.java
@@ -1,6 +1,9 @@
 
 package blockswarm;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author cal
@@ -11,8 +14,15 @@ public class BlockSwarm
 
     public static void main(String[] args)
     {
-        // Here we go!
-        new PeerTest();
-        //new FileTest();
+        try
+        {
+            // Here we go!
+            //new PeerTest();
+            //new FileTest(); 
+            new TestEchoServerMultiClient().testTwoClients();
+        } catch (Exception ex)
+        {
+            Logger.getLogger(BlockSwarm.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
