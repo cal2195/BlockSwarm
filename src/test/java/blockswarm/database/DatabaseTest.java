17a3a5d0e541b8f41f794394b40c0dcda11e2bbf
cal2195 <cmartin7@tcd.ie>
2016-03-25 19:44:57 +0000

    Renamed test package!

--- src/test/java/blockswarm/database/DatabaseTest.java
+++ src/test/java/blockswarm/database/DatabaseTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package blockswarm.test;
+package blockswarm.database;
 
 import blockswarm.database.Database;
 import org.junit.Test;
